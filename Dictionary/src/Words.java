28457833e4c899668dab6a5c34afe867dd9c3380
Michael Ryan <michael_ryan56@Michaels-Air.fios-router.home>
2020-03-13 13:39:06 -0400

    Getters and Setters #4

---
+++ Dictionary/src/Words.java
@@ -0,0 +1,14 @@
+public class Words {
+	private String word; 
+	private Definitions[] definitions; 
+	private String[] synonyms; 
+	private String[] antonyms; 
+	
+	public Words(String word, Definitions[] definitions, String[] synonyms, String[] antonyms) {
+		this.word = word;
+		this.definitions = definitions; 
+		this.synonyms = synonyms; 
+		this.antonyms = antonyms; 
+
+	}
+}
