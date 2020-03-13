3ab3f8a7ce6cc829c43be576e74ed8ee2839ba51
Michael Ryan <michael_ryan56@Michaels-Air.fios-router.home>
2020-03-13 12:28:34 -0400

    Added Buttons.

--- Dictionary/src/Frame.java
+++ Dictionary/src/Frame.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.BorderFactory; 
+import javax.swing.JTextField;
+import javax.swing.text.*;
+import javax.swing.event.*;
 
 // import com.google.gson.Gson; 
 
@@ -10,17 +13,41 @@ import java.util.*;
 
 public class Frame {
 	
-	public static void createAndShowUI() {
+	public static void createAndShowUI() throws BadLocationException {
 		JFrame Dictionary = new JFrame("Dictionary");
-		Dictionary.setPreferredSize(new Dimension(900, 750));
-		Dictionary.setResizable(false);
 		Dictionary.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dictionary.setPreferredSize(new Dimension(900, 750));
 		Dictionary.pack();
 		Dictionary.setLocationRelativeTo(null);
+		
+		JButton add = new JButton("Add");
+		add.setBounds(5, 5, 100, 35);
+		Dictionary.add(add);
+		
+		JButton remove = new JButton("Remove");
+		remove.setBounds(110, 5, 100, 35);
+		Dictionary.add(remove);
+		
+			
+		//JFrame Option(s)
+		Dictionary.setLayout(null);
 		Dictionary.setVisible(true);
+		Dictionary.setResizable(false);
+		
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		createAndShowUI();
-	}
+	  public static void main(String[] args){
+
+	      javax.swing.SwingUtilities.invokeLater(new Runnable(){ // Schedule a job
+	        // for the event-dispatching thread: <-- Received from Oracle's website.
+	        public void run(){
+	          try {
+				createAndShowUI();
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        }
+	      });
+	  }
 }
