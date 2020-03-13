26e335352589a7369a84314c197e87a7b512f468
Michael Ryan <michael_ryan56@Michaels-Air.fios-router.home>
2020-03-13 12:55:10 -0400

    Added JList and JTextField.

--- Dictionary/src/Frame.java
+++ Dictionary/src/Frame.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.BorderFactory; 
-import javax.swing.JTextField;
 import javax.swing.text.*;
 import javax.swing.event.*;
 
@@ -28,6 +27,10 @@ public class Frame {
 		remove.setBounds(110, 5, 100, 35);
 		Dictionary.add(remove);
 		
+		JTextField searchBar = new JTextField("Search");
+		searchBar.setBounds(5, 45, 207, 35);
+		Dictionary.add(searchBar);
+		
 		JRadioButton ascend = new JRadioButton("Asc");
 		ascend.setBounds(22, 80, 60, 30);
 		Dictionary.add(ascend);
@@ -36,11 +39,17 @@ public class Frame {
 		descend.setBounds(123, 80, 65, 30);
 		Dictionary.add(descend);
 		
-		
 		JTextArea definition = new JTextArea(); 
 		definition.setBounds(218, 5, 675, 715);
 		definition.setEditable(false);
 		Dictionary.add(definition);
+		
+		JList<String> words = new JList<String>();
+		words.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		words.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		words.setVisibleRowCount(-1);
+		words.setBounds(6, 123, 207, 597);
+		Dictionary.add(words);
 		
 		//JFrame Option(s)
 		Dictionary.setLayout(null);
