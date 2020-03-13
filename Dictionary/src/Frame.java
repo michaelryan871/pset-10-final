04de263fd10b5046f7c25b584f906db6acfa9b7f
Michael Ryan <michael_ryan56@Michaels-Air.fios-router.home>
2020-03-13 11:46:35 -0400

    JFrame.

--- Dictionary/src/Frame.java
+++ Dictionary/src/Frame.java
@@ -1,7 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.BorderFactory; 
+
+// import com.google.gson.Gson; 
+
+import java.io.*; 
+import java.util.*;
+
+
 public class Frame {
-		
 	
-	public static void main(String[] args) {
-		
+	public static void createAndShowUI() {
+		JFrame Dictionary = new JFrame("Dictionary");
+		Dictionary.setPreferredSize(new Dimension(900, 750));
+		Dictionary.setResizable(false);
+		Dictionary.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dictionary.pack();
+		Dictionary.setLocationRelativeTo(null);
+		Dictionary.setVisible(true);
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		createAndShowUI();
 	}
 }
